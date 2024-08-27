f8fa5e55d37455788cd22c00fc48819426ad9742
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-27 09:52:17 +0500

    add q2 of lab2

---
+++ LAB02/Q2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+
+
+            try{
+
+                System.out.println("Enter a number:");
+                String num = scanner.next();
+                int converted = Integer.parseInt(num);
+
+            }catch (NumberFormatException e){
+                System.out.println("Input is not integer.");
+            }
+
+
+            System.out.println("Enter a integer to be divided by zero:");
+            int num1 = scanner.nextInt();
+            int infinity = num1 / 0;
+
+        }catch (ArithmeticException e){
+            System.out.println("Arithmetic Exception");
+
+        }
+    }
+}
